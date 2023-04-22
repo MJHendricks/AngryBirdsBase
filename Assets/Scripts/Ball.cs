0a55b23e86a155c4503dbe84a9ca19154bd2bd2b
Matt <matthew.joshua.hendricks@gmail.com>
2023-04-22 16:05:04 +0200

    update 220423

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Ball : MonoBehaviour
+{
+    private bool isPressed = false;
+    public Rigidbody2D rb;
+    public Rigidbody2D hook;
+
+    
+    void Update()
+    {
+        if (isPressed)
+        {
+            Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if (Vector3.Distance(mousePos, hook.position) > 2)
+            {
+                rb.position = hook.position + (mousePos - hook.position).normalized * 2;
+            }
+            else 
+            {
+                rb.position = mousePos;
+            }
+            
+        }
+    }
+
+    public void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    public void OnMouseUp()
+    {
+        isPressed =false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(0.05f);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float health = 4f;
+
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.relativeVelocity.magnitude > health)
+        {
+            Die();
+        }
+
+    }
+
+    public void Die()
+    {
+        Destroy(gameObject);
+
+    }
+}
